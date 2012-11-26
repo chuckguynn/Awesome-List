650a85a355fe01c0a3a6c48e0ddfcd8a3897d0f2
Chuck Guynn <chuckguynn@me.com>
2012-11-26 13:42:35 -0600

    11-26-12

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ActiveRecord::Base
   attr_accessible :author, :description, :title, :user_id, :year, :image
   belongs_to :user
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, :path => ":rails_root/public/system/:attachment/:id/:style/:filename", :url => "/system/:attachment/:id/:style/:filename"
 end
