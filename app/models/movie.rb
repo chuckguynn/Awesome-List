cc21a2cf7065980312abf41621049df73602ad8b
Chuck Guynn <chuckguynn@me.com>
2012-11-26 13:49:44 -0600

    11-26-12

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ActiveRecord::Base
   attr_accessible :actors, :description, :director, :genre, :title, :user_id, :year, :image
   belongs_to :user
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, :path => ":rails_root/public/system/:attachment/:id/:style/:filename", :url => "/system/:attachment/:id/:style/:filename"
 end
--- app/models/music.rb
+++ app/models/music.rb
@@ -1,5 +1,5 @@
 class Music < ActiveRecord::Base
   attr_accessible :album, :artist, :comments, :favorite_song, :genre, :user_id, :image
   belongs_to :user
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, :path => ":rails_root/public/system/:attachment/:id/:style/:filename", :url => "/system/:attachment/:id/:style/:filename"
 end
